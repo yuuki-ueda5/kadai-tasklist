83640fb1c12be7754407a1c3747cf2e5e03c0e20
yuuki.ueda <yuuki.1673.b@gmail.com>
2021-06-28 14:59:51 +0900

    add getalltasks

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.tasks;
+import utils.DBUtil;
+
 
 /**
  * Servlet implementation class IndexServlet
@@ -26,7 +32,13 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createentityManager();
+
+        List<tasks> task = em.createNamedQuery("getAlltasks",tasks.class).getResultList();
+        response.getWriter().append(Integer.valueOf(task.size()).toString());
+
+        em.close();
+
     }
 
 }
--- src/models/tasks.java
+++ src/models/tasks.java
@@ -7,9 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAlltasks",
+            query = "SELECT m FROM tasks AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "task")
 
 public class tasks {
